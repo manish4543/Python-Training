a3015b0df24e17c7885b13e69213908c9fbf0d2c
manish4543 <164373271+manish4543@users.noreply.github.com>
2024-06-04 10:40:33 +0530

    added a Python File

---
+++ Day1.py
@@ -0,0 +1,92 @@
+# strings index
+#s1="mani"
+#print(s1[1])
+#print(s1[-2])
+#print(s1[1:3])
+#print(s1[-2:-4])
+
+#a=10.5
+#print(float(a))
+
+#a=int(input("enter a number"))
+#print(a)
+#print(type(a))
+
+#a=57
+##b=(bin(a))
+
+#a=10
+#print(id(a))
+#b=20
+#print(id(b))
+
+#list= [2,3,"Dog",True,9.99]
+#list.append("cat")
+#list.remove(3)
+#print(list)
+
+#set= {10,20,10,"cat",True, "cat"}
+#print(set)
+
+#operators
+
+#a=10.5
+#b=2
+#print("a+b=",a+b)
+#print("a-b=",a-b)
+#print("a*b=",a*b)
+#print("a/b=",a/b)
+#print("a//b=",a//b)
+#print("a%b=",a%b)
+#print("a**b=",a**b)
+
+#equality operator
+
+#a=10
+#b=20
+#print(a)
+#print(b)
+#print(a==b)
+
+#logical operator
+
+#bitwise operator
+
+#a=10
+#b=20
+#temp=a
+#a=b
+#b=temp
+#print(b)
+#print(a)
+#a,b=b,a
+#print(a)
+#print(b)
+
+#a=a^b
+#b=a^b
+#a=a^b
+#print(a)
+#print(b)
+
+
+#n= int(input())
+#if(n&1==0):
+#    print("even")
+#else:
+#    print("odd")
+
+#assignment opperator 
+
+
+# year=int(input())
+# if year%4==0:
+#     if year%100==0:
+#        if year%400==0:
+#             print(year,"is a leap year")
+#         else:
+#             print("not leap year")
+#     else:
+#         print(year,"leap year")
+# else:
+#     print("not a leap year")
